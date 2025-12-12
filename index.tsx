b44e12a362f0fd083e74ce6f8900e1bb3287cb97
Ingmar <Ingmarmeijer@gmail.com>
2025-12-12 21:21:16 +0100

    Restored StrictMode in index.tsx and removed commented-out CSS import

--- index.tsx
+++ index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import './index.css';  <-- DELETE THIS LINE
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // Temporarily disabled StrictMode to fix Three.js animation
-  // <React.StrictMode>
+  <React.StrictMode>
     <App />
-  // </React.StrictMode>
+  </React.StrictMode>
 );
